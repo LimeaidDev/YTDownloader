d8f18238ce77d59bc3471482c879382101f97e9f
Limeade <116500605+LimeaidDev@users.noreply.github.com>
2024-04-07 07:27:02 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -2,17 +2,27 @@ import os
 import pytube
 from pytube import YouTube
 from pytube.exceptions import RegexMatchError
+import os
+import string
+import random
 
+
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
+def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
 
 def download_video(url: str):
     try:
         print("Downloading...")
         yt = YouTube(url)
+        file = id_generator(10)
         video = yt.streams.filter(progressive=True).desc().first()
-        title = video.title
-        video.download("/Videos", filename=f"{video.title}.mp4")
+        video.download("./Videos", filename_prefix=f"{file}", filename=f".mp4")
         print("Downloaded!")
-        return "SUCCESS", title
+        return "SUCCESS", file
     except RegexMatchError:
         print("Invalid URL")
         return "ERROR"
@@ -24,5 +34,5 @@ def download_video(url: str):
 while True:
     returned = download_video(input("YT URL: "))
     if returned[0] == "SUCCESS":
-        os.system('"' + f'/Videos/{returned[1]}.mp4' + '"')
+        os.system('"' + f'.\\Videos\\{returned[1]}.mp4' + '"')
 
